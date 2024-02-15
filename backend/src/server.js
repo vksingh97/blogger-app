ccdfd12ac483af4c10aa986383abe78cb7a4f310
Vivek <vivek.kumar@uolo.com>
2024-02-15 17:25:29 +0530

    blogger app boiler plate

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes/routes');
+const path = require('path');
+const reponseHandlers = require('./middlewares/response');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+app.use(express.text({ limit: '25mb' }));
+app.use(express.json({ limit: '1mb' }));
+app.use(reponseHandlers);
+app.use(routes);
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className='App'>Blogging App</div>;
+}
+
+export default App;
